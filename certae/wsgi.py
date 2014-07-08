c9f3e76e1e684297b682d0a254f5aa3c0572117d
Victorette <giovanni.victorette@gmail.com>
2014-07-08 13:21:56 -0400

    change: Template adjustements for deploy

--- certae/wsgi.py
+++ certae/wsgi.py
@@ -7,8 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "certae.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+path = '/var/www/rai'
+if path not in sys.path:
+    sys.path.append(path)
